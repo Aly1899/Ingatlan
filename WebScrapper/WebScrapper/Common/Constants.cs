887ffc0fbf3d2f16735dd886d1a33f20b77ee7e4
Aly1899 <alpar1899@gmail.com>
2022-01-10 10:46:44 +0100

    feat: refact, uri added

--- WebScrapper/WebScrapper/Common/Constants.cs
+++ WebScrapper/WebScrapper/Common/Constants.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WebScrapper.Common
+﻿namespace WebScrapper.Common
 {
     public static class Constants
     {
         public const string House = "ház";
         public const string Flat = "lakás";
-        public const string Plot = "telek";
+        public const string Plot = "telek-L";
+        public const string PlotOther = "telek-E";
+        public const string HouseUri = "https://ingatlan.com/szukites/elado+haz+csak-kepes+20-80-mFt+500-m2telek-felett+4-szoba-felett";
+        public const string FlatUri = "https://ingatlan.com/szukites/elado+lakas+budapest+30-50-mFt+4-szoba-felett";
+        public const string PlotUri = "https://ingatlan.com/szukites/elado+telek+lakoovezeti-telek+30-mFt-ig";
+        public const string PlotOtherUri = "https://ingatlan.com/szukites/elado+telek+uduloovezeti-telek+kulteruleti-telek+egyeb-telek+10-mFt-ig";
     }
 }
